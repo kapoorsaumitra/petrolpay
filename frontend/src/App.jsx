d6bdfebbd4e08b85388183eb44ed783dc7cc99a5
kapoorsaumitra <saumitrakapoor24@gmail.com>
2024-03-31 06:36:01 +0530

    changed route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
@@ -11,16 +7,16 @@ import { SendMoney } from "./pages/SendMoney";
 function App() {
   return (
     <>
-       <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
+          <Route path={["/signup", "/"]} element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
